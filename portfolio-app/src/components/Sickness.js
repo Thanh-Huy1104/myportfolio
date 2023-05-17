b232dec9026a44c634decc37b7e85644027c2f08
TH <etienne01.nguyen@gmail.com>
2023-05-17 17:58:35 -0400

    added sicknesshacks text

--- portfolio-app/src/components/Sickness.js
+++ portfolio-app/src/components/Sickness.js
@@ -3,7 +3,7 @@ import Sickness_img from '../assets/img/SicknessHacks.png'
 
 const RedBird = () => {
   return (
-    <Project text="RedBirdsRoster streamlines task management with a user-friendly design and pleasant interface. Say goodbye to cluttered to-do lists, confusing event planning, and accessible team management and hello to an intuitive platform that prioritizes ease of use and efficiency. Enhance your team's productivity and get more done in less time with our innovative solution." title="Sickness Hacks | AI Project" img={Sickness_img} link="https://devpost.com/software/salle-d-urgence-declogger?ref_content=user-portfolio&ref_feature=in_progress"/>
+    <Project text="Introducing our web app, the solution to Quebec's overcrowded emergency rooms during the Covid pandemic. Say goodbye to long waits and uncertainty. Simply enter your symptoms and receive a preliminary prediction on ur health, taking control of your health. With our AI model, you will receive a set of precautions to take and a score determining if you do need to go to the hostpital." title="Sickness Hacks | AI Project" img={Sickness_img} link="https://devpost.com/software/salle-d-urgence-declogger?ref_content=user-portfolio&ref_feature=in_progress"/>
   )
 }
 
